4430ceb25101487f3a6d1ac40e16841674553b83
Tobias Knopp <tobias@knoppweb.de>
2018-10-29 12:34:51 +0100

    update travis

--- docs/make.jl
+++ docs/make.jl
@@ -4,16 +4,14 @@ makedocs(
     format = :html,
     modules = [RegularizedLeastSquares],
     sitename = "RegularizedLeastSquares.jl",
-    authors = "Tobias Knopp",
+    authors = "Tobias Knopp, Mirco Grosser, Martin Möddel",
     pages = [
         "Home" => "index.md",
         "Getting Started" => "gettingStarted.md",
         "Operator Interface" => "operators.md",
     ],
+    html_prettyurls = false, #!("local" in ARGS),
 )
 
 deploydocs(repo   = "github.com/tknopp/RegularizedLeastSquares.jl.git",
-           julia  = "0.7",
-           target = "build",
-           deps   = nothing,
-           make   = nothing)
+           target = "build")
