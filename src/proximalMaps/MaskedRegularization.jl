f6a005a3a69dd378080be7dd39f72fab65b37d56
nHackel <nHackel@users.noreply.github.com>
2023-08-31 17:02:32 +0200

    Rename constrained to masked regularization

---
+++ src/proximalMaps/MaskedRegularization.jl
@@ -0,0 +1,20 @@
+export MaskedRegularization
+
+struct MaskedRegularization{R<:AbstractRegularization} <: AbstractRegularization
+  reg::R
+  constraintMask::Vector{Bool}
+end
+λ(reg::MaskedRegularization) = λ(reg.reg)
+sink(reg::MaskedRegularization) = sink(reg.reg)
+
+
+function prox!(reg::MaskedRegularization, x::AbstractArray)
+	z = view(x, findall(reg.constraintMask))
+  prox!(reg.reg, z)
+	return x
+end
+function norm(reg::MaskedRegularization, x::AbstractArray)
+  z = view(x, findall(reg.constraintMask))
+  result = norm(reg.reg, z)
+  return result
+end
