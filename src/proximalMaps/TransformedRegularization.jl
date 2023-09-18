a29cf5f9ee1783a9f5dca0473fabf317088f0c9b
nHackel <nHackel@users.noreply.github.com>
2023-09-18 14:03:46 +0200

    Rename sparseTrafo field of TransformedReg

--- src/proximalMaps/TransformedRegularization.jl
+++ src/proximalMaps/TransformedRegularization.jl
@@ -1,32 +1,32 @@
 export TransformedRegularization
 
-struct TransformedRegularization{R<:AbstractRegularization, S} <: AbstractRegularization
+struct TransformedRegularization{R<:AbstractRegularization, TR} <: AbstractRegularization
   reg::R
-  sparseTrafo::S
+  trafo::TR
 end
 sink(reg::TransformedRegularization) = sink(reg.reg)
 λ(reg::TransformedRegularization) = λ(reg.reg)
 
 function prox!(reg::TransformedRegularization, x::AbstractArray)
-	z = reg.sparseTrafo * x
+	z = reg.trafo * x
   result = prox!(reg.reg, z)
-	x[:] = adjoint(reg.sparseTrafo) * result
+	x[:] = adjoint(reg.trafo) * result
   return x
 end
 function norm(reg::TransformedRegularization, x::AbstractArray)
-  z = reg.sparseTrafo * x 
+  z = reg.trafo * x 
   result = norm(reg.reg, z)
   return result
 end
 
 function prox!(reg::TransformedRegularization, x::AbstractArray, λ)
-	z = reg.sparseTrafo * x
+	z = reg.trafo * x
   result = prox!(reg.reg, z, λ)
-	x[:] = adjoint(reg.sparseTrafo) * result
+	x[:] = adjoint(reg.trafo) * result
   return x
 end
 function norm(reg::TransformedRegularization, x::AbstractArray, λ)
-  z = reg.sparseTrafo * x 
+  z = reg.trafo * x 
   result = norm(reg.reg, z, λ)
   return result
 end
