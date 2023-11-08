41df469ccedc297a0bf81fed1032b52269bb21fd
nHackel <nHackel@users.noreply.github.com>
2023-11-08 14:45:53 +0100

    Fix tests

--- src/CGNR.jl
+++ src/CGNR.jl
@@ -1,9 +1,10 @@
 export cgnr, CGNR
 
-mutable struct CGNR{matT,opT,vecT,T, R} <: AbstractKrylovSolver
+mutable struct CGNR{matT,opT,vecT,T, R, PR} <: AbstractKrylovSolver
   A::matT
   AᴴA::opT
   L2::R
+  constr::PR
   cl::vecT
   rl::vecT
   zl::vecT
@@ -70,19 +71,21 @@ function CGNR(A, x::vecT=zeros(eltype(A),size(A,2)); reg::Vector{R} = [L2Regular
     L2 = L2Regularization(zero(typeof(real(T))))
   else
     L2 = reg[idx]
+    deleteat!(reg, idx)
   end
-  deleteat!(reg, idx)
 
-  #indices = findsinks(AbstractProjectionRegularization, reg)
-  #other = [reg[i] for i in indices]
-  #deleteat!(reg, indices)
+  indices = findsinks(RealRegularization, reg)
+  push!(indices, findsinks(PositiveRegularization, reg)...)
+  other = [reg[i] for i in indices]
+  deleteat!(reg, indices)
   if length(reg) > 0
     error("CGNR does not allow for more additional regularization terms, found $(length(reg))")
   end
+  other = identity.(other)
 
 
   return CGNR(A, AᴴA,
-             L2,cl,rl,zl,pl,vl,xl,αl,βl,ζl,
+             L2,other,cl,rl,zl,pl,vl,xl,αl,βl,ζl,
              weights,iterations,relTol,0.0,normalizeReg)
 end
 
@@ -123,7 +126,6 @@ function init!(solver::CGNR, u::vecT
 
   # normalization of regularization parameters
   solver.L2 = normalize(solver, solver.normalizeReg, solver.L2, solver.A, u)
-  solver.reg = normalize(solver, solver.normalizeReg, solver.reg, solver.A, u)
 end
 
 """
@@ -162,9 +164,9 @@ performs one CGNR iteration.
 """
 function iterate(solver::CGNR, iteration::Int=0) 
     if done(solver,iteration)
-      #for r in solver.reg
-      #  prox!(r, solver.cl)
-      #end
+      for r in solver.constr
+        prox!(r, solver.cl)
+      end
       return nothing
     end
 
