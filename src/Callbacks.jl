d99422d701a48e125f6f926b1777462654753add
nHackel <nHackel@users.noreply.github.com>
2023-12-07 08:56:12 +0100

    Change from callback to callbacks and allow vector

--- src/Callbacks.jl
+++ src/Callbacks.jl
@@ -5,6 +5,11 @@ mutable struct CompareSolutionCallback{T, F}
   results::Vector{Float64}
 end
 
+"""
+    CompareSolutionCallback(ref, cmp)
+
+Callback that compares the solvers current `solution` with the given reference via `cmp(ref, solution)` per iteration. Results are stored in the `results` field.
+"""
 CompareSolutionCallback(ref::Vector{T}, cmp = nrmsd) where T = CompareSolutionCallback(ref, cmp, Vector{Float64}())
 
 function (cb::CompareSolutionCallback)(solver::AbstractLinearSolver, _)
@@ -16,6 +21,11 @@ export StoreSolutionCallback
 mutable struct StoreSolutionCallback{T}
   solutions::Vector{Vector{T}}
 end
+"""
+    StoreSolutionCallback(T)
+
+Callback that accumlates the solvers `solution` per iteration. Results are stored in the `solutions` field.
+"""
 StoreSolutionCallback(T::Type=Number) = StoreSolutionCallback(Vector{Vector{T}}())
 function (cb::StoreSolutionCallback)(solver::AbstractLinearSolver, _)
   x = solversolution(solver)
@@ -25,24 +35,18 @@ end
 export StoreConvergenceCallback
 mutable struct StoreConvergenceCallback
   convMeas::Dict{Symbol, Any}
-  StoreConvergenceCallback() = new(Dict{Symbol, Any}())
 end
+"""
+    StoreConvergenceCallback()
+
+Callback that accumlates the solvers convergence metrics per iteration. Results are stored in the `convMeas` field.
+"""
+StoreConvergenceCallback() = new(Dict{Symbol, Any}())
 function (cb::StoreConvergenceCallback)(solver::AbstractLinearSolver, _)
   meas = solverconvergence(solver)
   for key in keys(meas)
     values = get(cb.convMeas, key, Vector{typeof(meas[key])}())
     push!(values, meas[key])
     cb.convMeas[key] = values
-  end
-end
-
-export MultipleCallbacks
-mutable struct MultipleCallbacks
-  callbacks::Vector{Any}
-end
-MultipleCallbacks(args...) = MultipleCallbacks([args...])
-function (cb::MultipleCallbacks)(solver, iteration)
-  for callback in cb.callbacks
-    callback(solver, iteration)
   end
 end
