62ebc1eb5c767133853bbde0ac90758aee9633e4
SebastianFlassbeck <79124869+SebastianFlassbeck@users.noreply.github.com>
2024-04-03 17:43:09 -0400

    fix indent

--- src/proximalMaps/ProxLLR.jl
+++ src/proximalMaps/ProxLLR.jl
@@ -9,7 +9,7 @@ Regularization term implementing the proximal map for locally low rank (LLR) reg
 * `λ`                  - regularization paramter
 
 # Keywords
-* `shape::Tuple{Int}`         - dimensions of the image
+* `shape::Tuple{Int}`            - dimensions of the image
 * `blockSize::Tuple{Int}=(2,2)`  - size of patches to perform singular value thresholding on
 * `randshift::Bool=true`         - randomly shifts the patches to ensure translation invariance
 * `fullyOverlapping::Bool=false` - choose between fully overlapping block or non-overlapping blocks
