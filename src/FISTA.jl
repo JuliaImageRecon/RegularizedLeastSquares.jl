be66f411305db03d20479270e2d9c5b3646a9adc
Jakob Asslaender <jakob.asslaender@nyumc.org>
2024-06-28 11:50:34 -0400

    increase accuracy of power iterations and remove flag `normalize_rho`

--- src/FISTA.jl
+++ src/FISTA.jl
@@ -22,8 +22,8 @@ mutable struct FISTA{rT <: Real, vecT <: Union{AbstractVector{rT}, AbstractVecto
 end
 
 """
-    FISTA(A; AHA=A'*A, reg=L1Regularization(zero(real(eltype(AHA)))), normalizeReg=NoNormalization(), rho=0.95, normalize_rho=true, theta=1, relTol=eps(real(eltype(AHA))), iterations=50, restart = :none, verbose = false)
-    FISTA( ; AHA=,     reg=L1Regularization(zero(real(eltype(AHA)))), normalizeReg=NoNormalization(), rho=0.95, normalize_rho=true, theta=1, relTol=eps(real(eltype(AHA))), iterations=50, restart = :none, verbose = false)
+    FISTA(A; AHA=A'*A, reg=L1Regularization(zero(real(eltype(AHA)))), normalizeReg=NoNormalization(), iterations=50, verbose = false, rho = 0.95 / power_iterations(AHA), theta=1, relTol=eps(real(eltype(AHA))), restart = :none)
+    FISTA( ; AHA=,     reg=L1Regularization(zero(real(eltype(AHA)))), normalizeReg=NoNormalization(), iterations=50, verbose = false, rho = 0.95 / power_iterations(AHA), theta=1, relTol=eps(real(eltype(AHA))), restart = :none)
 
 creates a `FISTA` object for the forward operator `A` or normal operator `AHA`.
 
@@ -37,8 +37,7 @@ creates a `FISTA` object for the forward operator `A` or normal operator `AHA`.
 * `precon`                                            - preconditionner for the internal CG algorithm
 * `reg::AbstractParameterizedRegularization`          - regularization term; can also be a vector of regularization terms
 * `normalizeReg::AbstractRegularizationNormalization` - regularization normalization scheme; options are `NoNormalization()`, `MeasurementBasedNormalization()`, `SystemMatrixBasedNormalization()`
-* `rho::Real`                                         - step size for gradient step
-* `normalize_rho::Bool`                               - normalize step size by the largest eigenvalue of `AHA`
+* `rho::Real`                                         - step size for gradient step; the default is `0.95 / max_eigenvalue` as determined with power iterations.
 * `theta::Real`                                       - parameter for predictor-corrector step
 * `relTol::Real`                                      - tolerance for stopping criterion
 * `iterations::Int`                                   - maximum number of iterations
@@ -53,13 +52,12 @@ function FISTA(A
              ; AHA = A'*A
              , reg = L1Regularization(zero(real(eltype(AHA))))
              , normalizeReg = NoNormalization()
-             , rho = 0.95
-             , normalize_rho = true
+             , iterations = 50
+             , verbose = false
+             , rho = 0.95 / power_iterations(AHA; verbose)
              , theta = 1
              , relTol = eps(real(eltype(AHA)))
-             , iterations = 50
              , restart = :none
-             , verbose = false
              )
 
   T  = eltype(AHA)
@@ -70,10 +68,6 @@ function FISTA(A
   xᵒˡᵈ = similar(x)
   res  = similar(x)
   res[1] = Inf # avoid spurious convergence in first iterations
-
-  if normalize_rho
-    rho /= abs(power_iterations(AHA))
-  end
 
   # Prepare regularization terms
   reg = isa(reg, AbstractVector) ? reg : [reg]
